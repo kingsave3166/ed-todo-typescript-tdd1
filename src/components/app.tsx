06e8f2ce46fd053daa709bec24dee1a8c4048e78
kingsave3166 <kingsave3166@gmail.com>
2018-09-30 17:40:17 -0700

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import "./app.css";
+
+//import uuid from "uuid";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> testing react typescript!</h1>
+      </div>
+    );
+  }
+}
+export default App;
